771068c8e4263bd4cb723cafb5e5a74e7bb0c038
Indra Kumar <indravip3@gmail.com>
2024-06-03 10:20:30 +0530

    added an env file

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://indravip3:indraKP77@cluster0.nxgpqcn.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.DATABASE_URL).then(()=>console.log("DB Connected"));
 }
